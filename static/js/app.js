99c02e57583a42fd7c46770d7bebc75c38270f08
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-01 14:44:04 +0200

    add displaying data from website

---
+++ static/js/app.js
@@ -0,0 +1,46 @@
+let allPlanets = document.querySelector("#planets");
+let page = 1;
+
+
+function downloadPlanets(url) {
+    fetch(url)
+        .then((response) => response.json())
+        .then((planets) => {
+            displayPlanets(planets)
+        })
+}
+
+
+function displayPlanets(data) {
+    allPlanets.innerHTML = `
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>DIAMETER</th>
+                        <th>CLIMATE</th>
+                        <th>TERRAIN</th>
+                        <th>SURFACE WATER</th>
+                        <th>POPULATION</th>
+                    </tr>
+                </thead>
+                `
+    let tBody = document.createElement('tbody');
+    tBody.setAttribute("id", "tbody");
+    allPlanets.appendChild(tBody)
+    data.results.forEach((detail, index) => {
+        document.querySelector("#tbody")
+            .innerHTML += `<tr>
+                               <td>${index + 1}</td>
+                               <td>${detail.name}</td>
+                               <td>${detail.diameter}</td>
+                               <td>${detail.climate}</td>
+                               <td>${detail.terrain}</td>
+                               <td>${detail.surface_water}</td>
+                               <td>${detail.population}</td>
+                           </tr>`
+    })
+}
+
+
+downloadPlanets(`https://swapi.dev/api/planets/?page=${page}`)
