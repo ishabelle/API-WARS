2d02d5c23c0100c01f1145dd392da12f9a05b939
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-02 12:34:24 +0200

    add function to add new user to database:

--- db_manager.py
+++ db_manager.py
@@ -2,4 +2,23 @@ from psycopg2.extras import RealDictCursor
 import db_common
 
 
+@db_common.connection_handler
+def check_exists_username(cursor: RealDictCursor, username):
+    query = """
+    SELECT * FROM users
+    WHERE username = %(username)s;
+    """
+    cursor.execute(query, {"username": username})
+    return cursor.fetchone()
 
+
+@db_common.connection_handler
+def add_user(cursor: RealDictCursor, new_user):
+    if check_exists_username(new_user["username"]):
+        return False
+    else:
+        query = """
+        INSERT INTO users (username, password, submission_time)
+        VALUES (%s, %s, now())
+        """
+        cursor.execute(query, (new_user["username"], new_user["password"]))
