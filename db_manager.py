f90327103f010d301bd2887e313c952bf60b615f
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-02 15:11:51 +0200

    refactor login user

--- db_manager.py
+++ db_manager.py
@@ -1,27 +1,44 @@
+import bcrypt
 from psycopg2.extras import RealDictCursor
 import db_common
+import datetime
+
+
+def get_submission_time():
+    time = datetime.datetime.now()
+    return time.strftime('%Y-%m-%d %H:%M:%S')
 
 
 @db_common.connection_handler
-def check_exists_username(cursor: RealDictCursor, username):
+def check_username_exists(cursor: RealDictCursor, username: str):
     query = """
-    SELECT * FROM users
-    WHERE username = %(username)s;
-    """
-    cursor.execute(query, {"username": username})
+        SELECT * FROM users
+        WHERE username = %(username)s;
+         """
+    cursor.execute(query, {
+        'username': username
+    })
     return cursor.fetchone()
 
 
 @db_common.connection_handler
-def add_user(cursor: RealDictCursor, new_user):
-    if check_exists_username(new_user["username"]):
+def register_user(cursor: RealDictCursor, username: str, seen_password: str, submission_time: int):
+    if check_username_exists(username):
         return False
-    else:
-        query = """
-        INSERT INTO users (username, password, submission_time)
-        VALUES (%s, %s, now());
-        """
-        cursor.execute(query, (new_user["username"], new_user["password"]))
+    query = """
+    INSERT INTO users (username, password, submission_time)
+    VALUES (%(username)s, %(password)s, %(submission_time)s)
+           """
+    return cursor.execute(query, {
+        'username': username,
+        'password': encrypt_password(seen_password),
+        'submission_time': submission_time
+    })
+
+
+def encrypt_password(password):
+    hashed_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_pass.decode('utf-8')
 
 
 @db_common.connection_handler
@@ -32,3 +49,18 @@ def get_user_details(cursor: RealDictCursor, username):
     """
     cursor.execute(query, {'username': username})
     cursor.fetchone()
+
+
+@db_common.connection_handler
+def check_user(cursor: RealDictCursor, username: str):
+    query = """
+        SELECT id, password
+        FROM users
+        WHERE username ILIKE %(username)s;
+    """
+    cursor.execute(query, {'username': username})
+    return cursor.fetchone()
+
+
+def verify_password(text_password, hashed_pass):
+    return bcrypt.checkpw(text_password.encode('utf-8'), hashed_pass.encode('utf-8'))
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for, flash
+from datetime import datetime
+from flask import Flask, render_template, redirect, request, url_for, session
 import os
-
 import db_manager
-import password_generator
 
 
 app = Flask(__name__)
@@ -19,16 +18,19 @@ def registration_page():
     return render_template("registration_page.html")
 
 
-@app.route("/registration", methods=["POST"])
-def register_new_user():
-    new_user = request.form.to_dict()
-    hashed_password = password_generator.generate_hash_password(request.form.get('password'))
-    new_user['password'] = hashed_password
-    if db_manager.add_user(new_user) is False:
-        return redirect(url_for('registration_page'))
-    db_manager.add_user(new_user)
-    print("User registered")
-    return redirect(url_for('index_page'))
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    if 'username' in session:
+        return redirect(url_for("display_questions_list"))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        submission_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if db_manager.register_user(username, password, submission_time) is False:
+            print('NOT REGISTERED')
+        db_manager.register_user(username, password, submission_time)
+        return redirect(url_for('login'))
+    return render_template('registration_page.html')
 
 
 @app.route("/login")
@@ -36,20 +38,22 @@ def login_page():
     return render_template("login_page.html")
 
 
-@app.route("/login", methods=["POST"])
-def login_user():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    user_details = db_manager.get_user_details(username)
-    if not user_details:
-        flash("INVALID USERNAME OR PASSWORD")
-        return redirect(url_for('login_page'))
-    else:
-        verified_password = password_generator.check_hash_password(user_details['password'], password)
-    if not verified_password:
-        flash("INVALID PASSWORD")
-    else:
-        return redirect(url_for('index_page'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('display_questions_list'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        typed_password = request.form.get('password')
+        user = db_manager.check_user(username)
+        if user and db_manager.verify_password(typed_password, user['password']):
+            session['user_id'] = user['id']
+            session['username'] = username
+            print('LOGGED IN')
+            return redirect('/')
+        else:
+            print('INVALID USERNAME OR PASSWORD')
+    return render_template('login_page.html')
 
 
 if __name__ == "__main__":
