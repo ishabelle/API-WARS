b42232d090fdd9ec32065ca638489b973672e3e3
ishabelle <kolodzinska.izabela@gmail.com>
2021-04-04 15:02:08 +0200

    add new table in database

--- database/database_api_wars.sql
+++ database/database_api_wars.sql
@@ -8,3 +8,15 @@ CREATE TABLE users (
     password varchar(500) NOT NULL,
     submission_time timestamp without time zone
 );
+
+DROP TABLE IF EXISTS public.planet_votes CASCADE;
+
+CREATE TABLE planet_votes(
+    id SERIAL PRIMARY KEY NOT NULL,
+    planet_id varchar(30) NOT NULL,
+    planet_name varchar(50) NOT NULL,
+    user_id INTEGER,
+    submission_time timestamp without time zone
+);
+
+ALTER TABLE ONLY planet_votes ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
